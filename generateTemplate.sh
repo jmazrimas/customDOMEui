b47cc0bac912cd794ea0390829a3d8dc35cc3768
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-05-26 10:18:50 -0500

    dropdown working

--- generateTemplate.sh
+++ generateTemplate.sh
@@ -2,3 +2,6 @@
 cp inputTemplateLong.html inputTemplate.html
 sed -i '/jquery.com/d' inputTemplate.html
 sed -i ':a;N;$!ba;s/\n/ /g' inputTemplate.html
+
+cp inputTemplate.html inputTemplateJSON.html
+sed -i 's/\"/\\"/g' inputTemplateJSON.html
