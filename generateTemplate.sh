db3ea894933979555cf4430d7e42e5e60d3f79b6
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-05-26 10:40:10 -0500

    added output templates

--- generateTemplate.sh
+++ generateTemplate.sh
@@ -5,3 +5,11 @@ sed -i ':a;N;$!ba;s/\n/ /g' inputTemplate.html
 
 cp inputTemplate.html inputTemplateJSON.html
 sed -i 's/\"/\\"/g' inputTemplateJSON.html
+
+#! /bin/bash
+cp outputTemplateLong.html outputTemplate.html
+sed -i '/jquery.com/d' outputTemplate.html
+sed -i ':a;N;$!ba;s/\n/ /g' outputTemplate.html
+
+cp outputTemplate.html outputTemplateJSON.html
+sed -i 's/\"/\\"/g' outputTemplateJSON.html
